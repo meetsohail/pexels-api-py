10a6229fbdb64c1ea4a346ecf8a8e75e4db54b0a
Sohail Ahmed <sohail.comsians@gmail.com>
2021-08-20 18:56:55 +0500

    added tests

---
+++ tests/test_pexels.py
@@ -0,0 +1,27 @@
+from pexels_api.pexels_api import Pexel
+
+pexels = Pexel('PASTE_API_KEY')
+
+
+def test_search_photos():
+    pexels.search_photos('ocean')
+
+
+def test_curated_photos():
+    pexels.curated_photos()
+
+
+def test_get_photo():
+    pexels.get_photo(3573351)
+
+
+def test_search_videos():
+    pexels.search_videos()
+
+
+def test_popular_videos():
+    pexels.popular_videos()
+
+
+def test_get_video():
+    pexels.get_video(1)
